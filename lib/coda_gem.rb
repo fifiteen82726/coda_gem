56e902bb45a168ca949811aceeb5722b611261b2
fifiteen82726 <fifiteen82726@gmail.com>
2016-07-18 23:36:25 +0800

    create pass

--- lib/coda_gem.rb
+++ lib/coda_gem.rb
@@ -2,4 +2,10 @@ require "coda_gem/version"
 
 module CodaGem
   # Your code goes here...
+  def self.create(name)
+  	
+  	
+  end
+
+
 end
---
+++ spec/coda_gem_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe CodaGem do
+	it 'create a single module' do
+		CodaGem.create('Fruit')
+	end
+end
